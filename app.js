06f5452cc0ee27f5ee9be5e3167c086c11221069
Kalum Gunawardhana <kguna415@gmail.com>
2025-10-28 06:32:45 +0530

    commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    res.json([{ id: 1, name: 'Kalum' }, { id: 2, name: 'Tharindu' }]);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.post('/users', (req, res) => {
+    const user = req.body;
+    console.log('New user:', user);
+    res.status(201).json({ message: 'User created', user });
 });
+
+app.listen(4000, () => console.log('Server running on http://localhost:4000'));
